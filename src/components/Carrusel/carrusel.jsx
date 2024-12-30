bffbb05e88940e16ef3f0bb506ffff1d2b08a9b2
Federico <boomarts@yahoo.com>
2024-12-30 19:31:01 -0300

    PROYECTO TERMINADO CON REACT - E COMMERCE

--- src/components/Carrusel/carrusel.jsx
+++ src/components/Carrusel/carrusel.jsx
@@ -2,11 +2,11 @@ const Carrusel = () => {
     return (
         <div className="carrusel">
             <div className="carrusel-images">
-                <div className="image"><img src="/public/carrusel/1.jpg" alt="1" /></div>
-                <div className="image"><img src="/public/carrusel/2.jpg" alt="2" /></div>
-                <div className="image"><img src="/public/carrusel/3.jpg" alt="3" /></div>
-                <div className="image"><img src="/public/carrusel/4.jpg" alt="4" /></div>
-                <div className="image"><img src="/public/carrusel/5.jpg" alt="5" /></div>
+                <div className="image"><img src="/carrusel/1.jpg" alt="1" /></div>
+                <div className="image"><img src="/carrusel/2.jpg" alt="2" /></div>
+                <div className="image"><img src="/carrusel/3.jpg" alt="3" /></div>
+                <div className="image"><img src="/carrusel/4.jpg" alt="4" /></div>
+                <div className="image"><img src="/carrusel/5.jpg" alt="5" /></div>
             </div>
         </div>
     )
